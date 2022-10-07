450d8dd78d0d0b9f211c193c301d41e45ba974ce
unknown <kalle.kinnunen@myy.haaga-helia.fi>
2022-10-07 15:56:03 +0300

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts'
+
 
 export default function App() {
+  const [contacts, setContacts] = useState([])
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      if (data.length > 0) {
+        setContacts(data)
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList 
+        data={contacts}
+        renderItem={({item}) => {
+          return (
+            <View>
+            <Text>{`${item.name}, ${item.phoneNumbers ? item.phoneNumbers[0].number : ''}`}</Text>
+            </View>
+          )
+        }}
+      />
+    <TouchableOpacity style={styles.button} onPress={() => getContacts()}><Text style={{ fontSize: 18 }}>Contacts</Text></TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 25,
   },
+  button: {
+    borderColor: 'red',
+    borderWidth: 10,
+    padding: 100,
+    borderRadius: 500,
+    backgroundColor: 'white'
+
+  }
 });
